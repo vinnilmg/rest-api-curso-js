9769bffdf56e43c3d2e86e327da7a99c91d5a536
Vinicius Lima <vinicius.gomes@sulamerica.com.br>
2022-08-06 11:45:41 -0300

    fix

--- src/app.js
+++ src/app.js
@@ -1,32 +1,29 @@
-import dotenv from 'dotenv';
-import express from 'express';
-import { resolve } from 'path';
-import cors from 'cors';
-import helmet from 'helmet';
+import dotenv from "dotenv";
+import express from "express";
+import { resolve } from "path";
+import cors from "cors";
+import helmet from "helmet";
 
-import tokenRoutes from './routes/tokenRoutes';
-import usuarioRoutes from './routes/usuarioRoutes';
-import alunoRoutes from './routes/alunoRoutes';
-import fotoRoutes from './routes/fotoRoutes';
+import tokenRoutes from "./routes/tokenRoutes";
+import usuarioRoutes from "./routes/usuarioRoutes";
+import alunoRoutes from "./routes/alunoRoutes";
+import fotoRoutes from "./routes/fotoRoutes";
 
-import './database/connection';
+import "./database/connection";
 
 dotenv.config();
 
 // Configuração de IPs para o CORS
-const whiteList = [
-	'http://localhost:3000',
-	'https://www.google.com',
-];
+const whiteList = ["http://localhost:3000", "https://www.google.com"];
 
 // Configuração de permissão CORS
 const corsOptions = {
 	origin(origin, callback) {
-		console.log(origin);
+		// console.log(origin);
 		if (whiteList.indexOf(origin) !== -1 || !origin) {
 			callback(null, true);
 		} else {
-			callback(new Error('Now allowed by CORS.'));
+			callback(new Error("Now allowed by CORS."));
 		}
 	},
 };
@@ -43,14 +40,14 @@ class App {
 		this.app.use(helmet());
 		this.app.use(express.urlencoded({ extended: true }));
 		this.app.use(express.json());
-		this.app.use(express.static(resolve(__dirname, '..', 'uploads'))); // arquivos estaticos
+		this.app.use(express.static(resolve(__dirname, "..", "uploads"))); // arquivos estaticos
 	}
 
 	routes() {
-		this.app.use('/tokens/', tokenRoutes);
-		this.app.use('/usuarios/', usuarioRoutes);
-		this.app.use('/alunos/', alunoRoutes);
-		this.app.use('/fotos/', fotoRoutes);
+		this.app.use("/tokens/", tokenRoutes);
+		this.app.use("/usuarios/", usuarioRoutes);
+		this.app.use("/alunos/", alunoRoutes);
+		this.app.use("/fotos/", fotoRoutes);
 	}
 }
 
