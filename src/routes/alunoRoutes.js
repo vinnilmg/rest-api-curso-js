320fb6f09e2b33cf0e0150bb60e70537ed24af4d
Vinicius Lima <vinicius.gomes@sulamerica.com.br>
2022-08-06 11:50:02 -0300

    fix quotes

--- src/routes/alunoRoutes.js
+++ src/routes/alunoRoutes.js
@@ -1,14 +1,14 @@
-import { Router } from "express";
-import alunoController from "../controllers/AlunoController";
-import loginRequired from "../middlewares/loginRequired";
-import idAlunoRequired from "../middlewares/alunoMiddleware";
+import { Router } from 'express';
+import alunoController from '../controllers/AlunoController';
+import loginRequired from '../middlewares/loginRequired';
+import idAlunoRequired from '../middlewares/alunoMiddleware';
 
 const router = new Router();
 
-router.get("/", alunoController.index);
-router.post("/", loginRequired, alunoController.create);
-router.put("/:id", loginRequired, idAlunoRequired, alunoController.update);
-router.get("/:id", loginRequired, idAlunoRequired, alunoController.show);
-router.delete("/:id", loginRequired, idAlunoRequired, alunoController.delete);
+router.get('/', alunoController.index);
+router.post('/', loginRequired, alunoController.create);
+router.put('/:id', loginRequired, idAlunoRequired, alunoController.update);
+router.get('/:id', loginRequired, idAlunoRequired, alunoController.show);
+router.delete('/:id', loginRequired, idAlunoRequired, alunoController.delete);
 
 export default router;
